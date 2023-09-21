1cb9cb8963480c8a4cb5e8202a9b224ffa557ae0
Tanguy9862 <tanguy98.surowiec@gmail.com>
2023-09-21 23:26:57 +0200

    update requirements

--- app.py
+++ app.py
@@ -4,21 +4,7 @@ import dash_mantine_components as dmc
 
 # LAYOUT
 from assets.footer import footer
-
 from pages.nav import navbar
-
-# TODO :
-# En faire un vrai projet data science :
-    # scrapper les donnnées sur le site original
-    # data pre processing
-    # utiliser wikipedia api
-    # faire un readme sur github, expliquer la démarche
-# Seconde page, animation ? utiliser eventlistener de dash extension ? ou bien callback basique comme j'avais fait?
-# Historique : constellation, 1 pt = 1 année
-    # description à droite à la place de la terre : titre, image, pays, desc.
-    # slider année
-    # dropdown constelellation
-# Tout le pre processing de choropleth : générer un fichier csv déjà préparé et propre
 
 app = dash.Dash(
     __name__,
