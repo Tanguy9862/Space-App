548476052ecfc64c882781fdbd0846b84a276e55
Tanguy9862 <tanguy98.surowiec@gmail.com>
2025-01-23 17:36:39 +0100

    Edit filename for historical facts data

--- user_config.py
+++ user_config.py
@@ -5,7 +5,7 @@ DEFAULT_ENVIRONMENT = 'local'
 
 # File and directory names
 DATA_DIRECTORY_NAME = 'data'
-HISTORICAL_FACTS_FILENAME = 'historical_facts_data.json'
+HISTORICAL_FACTS_FILENAME = 'historical_facts.json'
 PAST_LAUNCHES_FILENAME = 'nsf_past_launches.csv'
 NEXT_LAUNCH_FILENAME = 'nsf_next_launch.json'
 
