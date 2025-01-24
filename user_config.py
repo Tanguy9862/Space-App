8f6fde5560b410b73e331ac7c87afcbafc488f7f
Tanguy9862 <tanguy98.surowiec@gmail.com>
2025-01-24 04:53:44 +0100

    Delete cloud configs

--- user_config.py
+++ user_config.py
@@ -1,5 +1,3 @@
-import os
-
 # Default environment that will be used if none is specified (used in config_loader.py)
 DEFAULT_ENVIRONMENT = 'local'
 
@@ -8,18 +6,3 @@ DATA_DIRECTORY_NAME = 'data'
 HISTORICAL_FACTS_FILENAME = 'historical_facts.json'
 PAST_LAUNCHES_FILENAME = 'nsf_past_launches.csv'
 NEXT_LAUNCH_FILENAME = 'nsf_next_launch.json'
-
-# AWS Configuration (if running in an AWS environment)
-AWS_BUCKET_NAME = 'app-space-exploration-bucket'
-AWS_SECRET_NAME = 'space-exploration-app/environment-config'
-AWS_REGION_NAME = 'us-east-1'
-
-# GCP Configuration (if running in a GCP environment)
-GCP_BUCKET_NAME = 'space-exploration-bucket-test'
-GCP_SECRET_NAME = 'space-exploration-secret-env-config'
-GCP_PROJECT_ID = 'spacexploration'
-
-# Local configuration to access GCP resources (only for development/testing)
-# This sets the path to the service account key JSON file for authentication.
-# If None, GCP authentication will rely on the default application credentials (ADC).
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'spacexploration-gcp-bucket-access.json' or None
